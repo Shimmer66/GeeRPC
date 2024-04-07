d1db73c15861ac74f51324c8e3f12e356d04a5c2
Shimmer66 <2279696120@qq.com>
2024-04-07 23:13:45 +0800

    first day

---
+++ server.go
@@ -0,0 +1,13 @@
+package GeeRPC
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int
+	CodecType   codec.Type
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
