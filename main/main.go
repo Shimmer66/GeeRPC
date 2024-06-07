8981538fce806f1f15cbbf3501770f0855c8ad4c
fengzhenn <fengzhenn@chanjet.com>
2024-06-07 11:09:52 +0800

    Correct the errors in the code above.

--- main/main.go
+++ main/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"geerpc"
 	"log"
 	"net"
@@ -12,39 +12,47 @@ import (
 
 type Foo int
 
-func startServer(addr chan string) {
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func startServer(addrCh chan string) {
 	var foo Foo
 	l, _ := net.Listen("tcp", ":9999")
 	_ = GeeRPC.Register(&foo)
 	GeeRPC.HandleHTTP()
-
-	addr <- l.Addr().String()
+	addrCh <- l.Addr().String()
 	_ = http.Serve(l, nil)
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-	client, _ := GeeRPC.Dial("tcp", <-addr)
-
+func call(addrCh chan string) {
+	client, _ := GeeRPC.DialHTTP("tcp", <-addrCh)
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
+	// send request & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
-
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
-				log.Fatal("call Foo.Sum error", err)
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
-		wg.Wait()
 	}
+	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
--- service.go
+++ service.go
@@ -20,23 +20,23 @@ func (m *methodType) NumCalls() uint64 {
 
 func (m *methodType) newArgv() reflect.Value {
 	var argv reflect.Value
+	// arg may be a pointer type, or a value type
 	if m.ArgType.Kind() == reflect.Ptr {
 		argv = reflect.New(m.ArgType.Elem())
 	} else {
-		argv = reflect.New(m.ReplyType).Elem()
+		argv = reflect.New(m.ArgType).Elem()
 	}
 	return argv
 }
 
 func (m *methodType) newReplyv() reflect.Value {
+	// reply must be a pointer type
 	replyv := reflect.New(m.ReplyType.Elem())
 	switch m.ReplyType.Elem().Kind() {
 	case reflect.Map:
 		replyv.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
-
 	case reflect.Slice:
 		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
-
 	}
 	return replyv
 }
@@ -53,9 +53,8 @@ func newService(rcvr interface{}) *service {
 	s.rcvr = reflect.ValueOf(rcvr)
 	s.name = reflect.Indirect(s.rcvr).Type().Name()
 	s.typ = reflect.TypeOf(rcvr)
-
 	if !ast.IsExported(s.name) {
-		log.Fatalf("rpc server")
+		log.Fatalf("rpc server: %s is not a valid service name", s.name)
 	}
 	s.registerMethods()
 	return s
@@ -81,12 +80,8 @@ func (s *service) registerMethods() {
 			ArgType:   argType,
 			ReplyType: replyType,
 		}
-		log.Printf("rpc server:register %s.%s\n", s.name, method.Name)
+		log.Printf("rpc server: register %s.%s\n", s.name, method.Name)
 	}
-}
-
-func isExportedOrBuiltinType(t reflect.Type) bool {
-	return ast.IsExported(t.Name()) || t.PkgPath() == ""
 }
 
 func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
@@ -97,4 +92,8 @@ func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
 		return errInter.(error)
 	}
 	return nil
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
 }
